a35ba998e0eadfe8450ccf5a4b576c7ae80309f9
LemonNeko <chheese048@gmail.com>
2023-04-27 01:54:31 +0800

    fix: add polyfil 2

--- wasm/index.ts
+++ wasm/index.ts
@@ -1,14 +1,26 @@
 import wasmInit from '../dist/easi-gen.wasm?init'
 
+globalThis.require = require;
+// @ts-ignore copied from $GOROOT/misc/wasm/wasm_exec_node.js
+globalThis.fs = require("fs");
+globalThis.TextEncoder = require("util").TextEncoder;
+globalThis.TextDecoder = require("util").TextDecoder;
+
+// @ts-ignore copied from $GOROOT/misc/wasm/wasm_exec_node.js
+globalThis.performance = {
+	now() {
+		const [sec, nsec] = process.hrtime();
+		return sec * 1000 + nsec / 1000000;
+	},
+};
+
 const crypto = require("crypto");
 globalThis.crypto = {
-    // @ts-ignore because it from go/misc/wasm
+    // @ts-ignore copied from $GOROOT/misc/wasm/wasm_exec_node.js
 	getRandomValues(b) {
 		crypto.randomFillSync(b);
 	},
 };
-
-import "../dist/wasm_exec.js"
 
 export const newEasiGen = async () => {
     const instance = await wasmInit({})
